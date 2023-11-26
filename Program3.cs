7245b49ad4085a1cce92fd5ad70e8e5f10c1028a
RadMa <maks25022@gmail.com>
2023-11-26 18:18:02 +0500

    old readme

---
+++ Program3.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter the number : ");
+string num = Console.ReadLine();
+int numr = int.Parse(num);
+
+int rem = numr % 2;
+
+if (rem == 0 )
+{
+    Console.WriteLine($"{numr} is even");
+}
+else 
+{
+    Console.WriteLine($"{numr} is odd");
+}
